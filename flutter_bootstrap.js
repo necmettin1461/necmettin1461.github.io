e0eca1d321693002a8c6581d1e8059491adb434e
necm1461 <necmeddinbera@gmail.com>
2025-05-08 00:42:59 +0200

    v2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4111725029"
+    serviceWorkerVersion: "2200676247"
   }
 });
